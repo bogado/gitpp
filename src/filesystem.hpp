531ebfd1c9c60bb9e2a416d9c3fc7eb680cf1025
Victor Bogado <victor@bogado.net>
2017-01-30 12:32:48 -0800

    Use checks to include the best version of the filesystem library.

--- src/filesystem.hpp
+++ src/filesystem.hpp
@@ -1,9 +1,25 @@
 #ifndef FILESYSTEM_HPP_INCLUDED
 #define FILESYSTEM_HPP_INCLUDED
 
-#include <experimental/filesystem>
+#if __has_include(<filesystem>)
+#include <filesystem>
 
 namespace git::fs {
+    using namespace std::filesystem;
     using std::ifstream;
 }
+#elif __has_include(<experimental/filesystem>)
+#include <experimental/filesystem>
+namespace git::fs {
+    using namespace std::experimental::filesystem;
+    using std::ifstream;
+}
+#else
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+namespace git::fs {
+    using namespace boost::filesystem;
+}
+#endif
+
 #endif
