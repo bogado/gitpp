ad141738f1e01265c22ad5ce84a4e15c92e53177
Victor Bogado <victor@bogado.net>
2017-01-30 12:30:53 -0800

    Use non-ownining-pointers to make the iterator more compliant.
    To be able to assign operators we need to be able to change the
    object the iterator is reffering to.

--- src/indexed_iterator.hpp
+++ src/indexed_iterator.hpp
@@ -11,47 +11,97 @@ public:
     using reference = value_type&;
     using pointer   = value_type*;
 
+    using difference_type = decltype(index_type{} - index_type{});
+    using iterator_category = std::random_access_iterator_tag;
+
 private:
     index_type index;
-    SOURCE& source;
+    const SOURCE* source;
 
 public:
-    indexed_iterator(index_type start, SOURCE& ret) :
+    indexed_iterator(index_type start, const SOURCE& ret) :
         index(start),
-        source(ret) {}
+        source(&ret)
+    {
+    }
+
+    indexed_iterator(const indexed_iterator&) = default;
+    indexed_iterator(indexed_iterator&&) = default;
+    indexed_iterator& operator=(const indexed_iterator&) = default;
+    indexed_iterator& operator=(indexed_iterator&&) = default;
+
+    auto operator-(const indexed_iterator& dest) {
+        return index - dest.index;
+    }
+
+    auto operator+(index_type n) {
+        return indexed_iterator{index + n, *source};
+    }
+
+    auto operator-(index_type n) {
+        return indexed_iterator{index + n, *source};
+    }
+
+    auto operator+=(index_type n) {
+        index += n;
+    }
+
+    auto operator-=(index_type n) {
+        index -= n;
+    }
 
     auto operator++() {
         index++;
-        return this;
+        return *this;
     }
 
     auto operator++(int) {
-        auto result = *this;
+        auto result{*this};
         index++;
         return result;
     }
 
     auto operator--() {
         index--;
-        return this;
+        return *this;
     }
 
     auto operator--(int) {
-        auto result = *this;
+        auto result{*this};
         index--;
         return result;
     }
 
-    auto operator*() const {
-        return source[index];
+    auto operator*() {
+        return (*source)[index];
+    }
+
+    auto operator[](index_type i) {
+        return (*source)[index + i];
     }
 
     bool operator==(const indexed_iterator& other) const {
-        return &source == &other.source && index == other.index;
+        return source == other.source && index == other.index;
     }
 
     bool operator!=(const indexed_iterator& other) const {
         return !(*this == other);
+    }
+
+    bool operator<(const indexed_iterator& other) const {
+        return index < other.index;
+    }
+
+    bool operator>(const indexed_iterator& other) const {
+        return index > other.index;
+    }
+
+    bool operator>=(const indexed_iterator& other) const {
+        return index >= other.index;
+    }
+
+    bool operator<=(const indexed_iterator& other) const {
+        return index <= other.index;
     }
 
     index_type current_index() {
@@ -61,26 +111,30 @@ public:
 
 template <class SOURCE,
          class VALUE_TYPE,
-         class ITERATOR = indexed_iterator<SOURCE>>
+         class ITERATOR = indexed_iterator<SOURCE> >
 class index_iterable {
-    SOURCE& base;
 public:
-    index_iterable(SOURCE& b) :
-        base(b)
-    {}
-
     using value_type = VALUE_TYPE;
     using reference = value_type&;
     using pointer = value_type*;
     using iterator = ITERATOR;
 
+private:
+    const SOURCE& getSelf() const {
+         return *static_cast<const SOURCE*>(this);
+    }
+
+public:
+
     iterator begin() const {
-        return iterator {0, base};
+        return iterator {0, getSelf()};
     }
 
     iterator end() const {
-        return iterator {base.size(), base};
+        return iterator {getSelf().size(), getSelf()};
     }
+
+private:
 };
 
 }
